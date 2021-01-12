157e16400c6e50f856bc0f717a7f2a766ea00a4b
anthroyeti <77356367+anthroyeti@users.noreply.github.com>
2021-01-12 14:50:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# KWChinese01
+ Map showing locations of 1899 Chinese laundries in Key West, Florida.
---
+++ data/ChineseLaundryPoints18991900_2.js
@@ -0,0 +1,20 @@
+var json_ChineseLaundryPoints18991900_2 = {
+"type": "FeatureCollection",
+"name": "ChineseLaundryPoints18991900_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Owner": "Choo, Chas", "Str_Number": 926.0, "Str_Name": "Southard", "1900Census": "No" }, "geometry": { "type": "Point", "coordinates": [ -81.796345660033253, 24.558270825708302 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Fung, Willie", "Str_Number": 425.0, "Str_Name": "Simonton", "1900Census": "Yes" }, "geometry": { "type": "Point", "coordinates": [ -81.801572922529246, 24.55702835025826 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Kee, San", "Str_Number": 607.0, "Str_Name": "Elizabeth", "1900Census": "No" }, "geometry": { "type": "Point", "coordinates": [ -81.799268236071043, 24.556221625918223 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Lee, Chas", "Str_Number": 906.0, "Str_Name": "Fleming", "1900Census": "No" }, "geometry": { "type": "Point", "coordinates": [ -81.797767683349733, 24.559004818138089 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Lee, Chiun", "Str_Number": 901.0, "Str_Name": "Duval", "1900Census": "No" }, "geometry": { "type": "Point", "coordinates": [ -81.799779874642212, 24.551995776364258 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Lee, Jim", "Str_Number": 522.0, "Str_Name": "Duval", "1900Census": "Yes" }, "geometry": { "type": "Point", "coordinates": [ -81.80256485858834, 24.555749197586984 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Ling, George", "Str_Number": 203.0, "Str_Name": "Duval", "1900Census": "Yes" }, "geometry": { "type": "Point", "coordinates": [ -81.80501355110259, 24.55905799725474 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Lee, Quong", "Str_Number": 422.0, "Str_Name": "Whitehead", "1900Census": "No" }, "geometry": { "type": "Point", "coordinates": [ -81.804336915114774, 24.555439139682822 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Ling, George", "Str_Number": 408.0, "Str_Name": "Greene", "1900Census": "Yes" }, "geometry": { "type": "Point", "coordinates": [ -81.805906681225551, 24.558497562551597 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Lung, Wing", "Str_Number": 631.0, "Str_Name": "Eaton", "1900Census": "Yes" }, "geometry": { "type": "Point", "coordinates": [ -81.801344645097316, 24.558457247881222 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Wing, Qung", "Str_Number": 1202.0, "Str_Name": "Simonton", "1900Census": "No" }, "geometry": { "type": "Point", "coordinates": [ -81.796570771514141, 24.549856600414923 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Wo, On", "Str_Number": 928.0, "Str_Name": "Frances", "1900Census": "No" }, "geometry": { "type": "Point", "coordinates": [ -81.792474683169658, 24.555467364088127 ] } },
+{ "type": "Feature", "properties": { "id": null, "Owner": "Unknown", "Str_Number": 324.0, "Str_Name": "Whitehead", "1900Census": "??" }, "geometry": { "type": "Point", "coordinates": [ -81.805101722112383, 24.556504431639777 ] } }
+]
+}
